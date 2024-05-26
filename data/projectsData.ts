e54bfc2e493cf9a427ae1700c8feaee73a0b3de5
canhnd <canhnd15@gmail.com>
2024-05-27 00:03:49 +0700

    update projects page  info

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,15 +1,15 @@
 const projectsData = [
   {
     title: 'E-Wedding',
-    description: `Reimagining compliance and risk screening with solutions powered by network analytics. Exploring better ways to secure the financial system, improve human-machine collaboration and what it means to build a company.`,
-    imgSrc: '/static/img/ewedding-card.png',
+    description: `Đây là một ứng dụng quản lý (management application) sử dụng React.js, Styled-Component, React Query, Supabase để quản lý các thông tin liên quan đến khách mời, thiệp mời, tiền mừng, chi tiêu và các dịch vụ của một đám cưới.`,
+    imgSrc: '/static/img/projects/ewedding-card.png',
     href: 'https://www.cylynx.io',
   },
   {
     title: 'My personal blog',
-    description: `What this blog is based on! I maintain it seperately as a side project. An out of the box personal portfolio template, beautifully styled, completely customisable and built on one of the hottest frontend frameworks.`,
-    imgSrc: '/static/img/demo-twitter-card.png',
-    href: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+    description: `Đây là blog cá nhân của mình, nơi mình ghi lại những kiến thức, kinh nghiệm đúc kết được trong quá trình đi làm, cả về chuyên môn cùng những câu chuyện bên lề.`,
+    imgSrc: '/static/img/projects/myblog.png',
+    href: 'https://davidnguyenblog.vercel.app/',
   },
 ]
 
