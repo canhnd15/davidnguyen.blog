5101a9cfe12537e134bcccb0aed169c360aad93d
canhnd <canhnd15@gmail.com>
2024-05-27 00:24:32 +0700

    update project links

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,13 +3,13 @@ const projectsData = [
     title: 'E-Wedding',
     description: `Đây là một ứng dụng quản lý (management application) sử dụng React.js, Styled-Component, React Query, Supabase để quản lý các thông tin liên quan đến khách mời, thiệp mời, tiền mừng, chi tiêu và các dịch vụ của một đám cưới.`,
     imgSrc: '/static/img/projects/ewedding-card.png',
-    href: 'https://www.cylynx.io',
+    href: 'https://ewedding.vercel.app/',
   },
   {
     title: 'My personal blog',
     description: `Đây là blog cá nhân của mình, nơi mình ghi lại những kiến thức, kinh nghiệm đúc kết được trong quá trình đi làm, cả về chuyên môn cùng những câu chuyện bên lề.`,
     imgSrc: '/static/img/projects/myblog.png',
-    href: 'https://davidnguyenblog.vercel.app/',
+    href: 'https://github.com/canhnd15/davidnguyen.blog',
   },
 ]
 
