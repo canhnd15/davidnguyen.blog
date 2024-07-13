2a49b029a92554584196fb8ca26786d8a90e6456
canhnd <canhnd15@gmail.com>
2024-07-14 00:12:07 +0700

    update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Link from './Link'
+import Logo from '@/data/logo.svg'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
-import Image from './Image'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -14,8 +15,11 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3 flex flex-col items-center justify-center">
-              <h1 className="text-3xl font-bold">David Nguyen</h1>
-              {/* <h3 className="font-mono">{`< A minimalist software engineer />`}</h3> */}
+              {/* <h1 className="text-3xl font-bold">David Nguyen</h1> */}
+              <div className="mr-3">
+                <Logo />
+              </div>
+              {/* <h3 className="font-mono">{`< Sharing is the best way to learn! />`}</h3> */}
             </div>
           </div>
         </Link>
